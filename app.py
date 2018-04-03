e451527c096af2a5babb16a779ddce37b136e9f5
Столяров Алексей <stolyarov@info74.ru>
2018-04-03 10:09:38 +0500

    fixed link tag in HTML

--- app.py
+++ app.py
@@ -30,9 +30,7 @@ def generate_buzz():
     """
     page += "</ul>"
     page += "<hr>"
-    page += "<h2>my profile below:</h2>"
-    page += "<a href 'https://www.upwork.com/freelancers/~01a6738208d8e81707'>Hire me<hr>"
-
+    page += "<H2>my profile on Upwork: <a href 'https://www.upwork.com/freelancers/~01a6738208d8e81707'>Hire me</a></H2>"
     page += '</h1></body></html>'
     return page
 
